f5bed56b38382d825b623de5a859b3509c732b12
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-12-12 17:13:41 -0300

    translate message-errors

---
+++ lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'As credenciais fornecidas não foram encontradas.',
+    'password' => 'A credencial fornecida não é igual a cadastrada.',
+    'throttle' => 'Muitas tentativas de login falharam. Por favor tente novamente em: :seconds seconds.',
+
+];
---
+++ lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Sua senha foi resetada!',
+    'sent' => 'Te enviamos um link para resetar a senha! :)',
+    'throttled' => 'Por favor, espere antes de tentar novamente',
+    'token' => 'Este código é inválido.',
+    'user' => "Não conseguimos encontrar o e-mail informado.",
+
+];
